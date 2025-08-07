f7165513379d1e19abc2aa7c3b3928239f758fcb
rhodamined <rhodamined@gmail.com>
2025-08-07 11:08:45 -0400

    write file successful

--- server/scheduler.js
+++ server/scheduler.js
@@ -3,19 +3,27 @@
 // Create folder for the day and write file
 // File name to include timestamp of the hour
 
-
 // import my modules
 const carparks = require("./carpark");
 const sgtime = require("./sgtime");
 
 // import libraries
 const cron = require("node-cron");
+const fse = require('fs-extra');
 
 
 // IIFE FOR TESTING
 (async function() {
-    console.log(sgtime.getYYYYMMDD() + '_' + sgtime.getH())
+
+    let testtime = sgtime.getISOString();
+    console.log(testtime);
+    let newtime = new Date(testtime);
+    console.log(newtime);
     // await carparks.getAllCarparks();
+
+    const fileStr = getFileString();
+    const payload = {timestamp: sgtime.getISOString(), value: [1, 2, 3]}
+    writeFile(fileStr, payload);
 })();
 
 
@@ -26,7 +34,35 @@ async function task() {
 }
 
 
+
 // function: make directory for the day if one doesn't exist, then save file to directory
+
+
+// fs-extra library 
+// outputJson will create directories if they don't already exist
+// payload expected to be in javascript object format
+async function writeFile (file_str, payload) {
+  try {
+    // output to file
+    await fse.outputJson(file_str, payload)
+
+    // read it back to log it
+    const data = await fse.readJson(file_str)
+    console.log(data) 
+
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+function getFileString() {
+    const date = sgtime.getYYYYMMDD();
+    const hr = sgtime.getH();
+    const str = `./data/${date}/${date}T${hr}.json`
+
+    return str;
+}
+
 
 // function: save file to directory 
 // --- if directory for the day does not exist, make it
--- server/sgtime.js
+++ server/sgtime.js
@@ -1,6 +1,8 @@
 module.exports = {
     getYYYYMMDD,
-    getH
+    getHHMMSS,
+    getH,
+    getISOString
 }
 
 // handle time -- singapore time
@@ -13,9 +15,22 @@ function getYYYYMMDD() {
     const date = time_now.getDate(); 
     const month = time_now.getMonth()+1; // base function returns 0 to 11
 
-    let yyyymmdd = `${year}${twoDigit(month)}${twoDigit(date)}`;
+    let yyyymmdd = `${year}-${twoDigit(month)}-${twoDigit(date)}`;
     return yyyymmdd;
 }
+
+function getHHMMSS() {
+
+    // https://stackoverflow.com/questions/11124322/get-date-time-for-a-specific-time-zone-using-javascript
+    let time_now = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Singapore"})); // timezone ex: Asia/Jerusalem
+    const hour = time_now.getHours(); // base function returns 0 to 23
+    const min = time_now.getMinutes(); // base function returns 0 to 59
+    const sec = time_now.getSeconds(); // base function returns 0 to 59
+
+    let hhmmss = `${twoDigit(hour)}:${twoDigit(min)}:${twoDigit(sec)}`;
+    return hhmmss;
+}
+
 
 function getH() {
 
@@ -26,6 +41,30 @@ function getH() {
     let h = `${twoDigit(hour)}`;
     return h;
 }
+
+
+// returns format in YYYY-MM-DDTHH:MM:SS+offset 
+// e.g. for 10:42am EDT == 22:42PM SST, the string would be: 2025-08-07T22:53:38+08:00
+// can be parsed back to correct UTC timestamp using Date(isostring)
+// thank god SG does not do daylight savings
+function getISOString() {
+
+    // https://stackoverflow.com/questions/11124322/get-date-time-for-a-specific-time-zone-using-javascript
+    let time_now = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Singapore"})); // timezone ex: Asia/Jerusalem
+    const year = time_now.getFullYear();
+    const date = time_now.getDate(); 
+    const month = time_now.getMonth()+1; // base function returns 0 to 11
+    const hour = time_now.getHours(); // base function returns 0 to 23
+    const min = time_now.getMinutes(); // base function returns 0 to 59
+    const sec = time_now.getSeconds(); // base function returns 0 to 59
+
+    const timezone_offset = "+08:00"; // singapore is GMT+8
+
+    let str = `${year}-${twoDigit(month)}-${twoDigit(date)}T${twoDigit(hour)}:${twoDigit(min)}:${twoDigit(sec)}${timezone_offset}`;
+
+    return str;
+}
+
 
 // add leading 0 if there's only 1 digit
 function twoDigit(num) {

b29b3fea40beddb6e6986dea2555929efa3c02c5
rhodamined <rhodamined@gmail.com>
2025-08-21 23:06:12 -0400

    merge in subway methods

--- python_scripts/carpark_led_csv_and_json.py
+++ python_scripts/carpark_led_csv_and_json.py
@@ -25,11 +25,17 @@ if __name__ == "__main__":
         # Received date to process as an argument in format 'yyyy-mm-dd' e.g. '2025-08-11' for August 11, 2025
         date_str = sys.argv[1]
         
-        # directory to iterate over
+        # directory of carparks to iterate over
         carpark_directory = Path('./data/carpark_availability/' + date_str)  # set directory path
+        
+        # directory of carparks to iterate over
+        subway_directory = Path('./data/subway_crowdedness/' + date_str)  # set directory path
 
-        # path to ref: carpark IDs
+        # path to ref: carpark IDs to kml
         path_to_carpark_ids = './ref/SG_carpark_IDs.json'
+        
+        # path to ref: subway stations to kml
+        path_to_subway_kml = './ref/SG_Station_Subzone_Lookup.csv'
         
         # path to ref: subzone kml ref by region
         path_to_subzone_kml_ref_by_region = './ref/subzone_kml_ref_by_region.csv'
@@ -138,11 +144,11 @@ if __name__ == "__main__":
         new_id_total = len(carpark_all['CarParkID'].unique())
         percent_good = new_id_total/orig_id_total
         
-        print("IDs kept: " + str(new_id_total) + "/" + str(orig_id_total))
+        print("Carpark IDs kept: " + str(new_id_total) + "/" + str(orig_id_total))
 
         # exit the process if data isn't good enough
         if percent_good < tolerance:
-            sys.exit("This dataset is " + str(percent_good) + " good The tolerance is set to " + str(tolerance) + ".")
+            sys.exit("This carpark dataset is " + str(percent_good) + " good The tolerance is set to " + str(tolerance) + ".")
         
         # ------------------------------------------------ #
         # CLEAN CARPARK DF
@@ -190,44 +196,148 @@ if __name__ == "__main__":
         # ------------------------------------------------ #
         # MERGE AND SUMMARIZE AVAILABLE LOTS PER KML
         # ------------------------------------------------ #
-        grouped = carpark_all.groupby(['subzone_kml', 'hour'])
+        carpark_grouped = carpark_all.groupby(['subzone_kml', 'hour'])
 
-        summary = grouped.agg(avail_norm = ('avail_norm', 'mean'))
-        summary_df = pd.DataFrame(summary)
+        carpark_summary = carpark_grouped.agg(avail_norm = ('avail_norm', 'mean'))
+        carpark_summary = pd.DataFrame(carpark_summary)
 
-        summary_df = summary_df.reset_index()
+        carpark_summary = carpark_summary.reset_index()
         
         # ------------------------------------------------ #
         # REMAP <AVAIL PER KML> TO <LED VALUES 0-255>
         # ------------------------------------------------ #
-        summary_df['led_val'] = summary_df['avail_norm'] * 255
-        summary_df['led_val'] = summary_df['led_val'].astype('int64')
+        carpark_summary['led_val'] = carpark_summary['avail_norm'] * 255
+        carpark_summary['led_val'] = carpark_summary['led_val'].astype('int64')
         
+        
+        # ------------------------------------------------ #
+        # READ SUBWAY FILES
+        # ------------------------------------------------ #
+        
+        li = []
+
+        for file in subway_directory.iterdir():  
+            if file.is_file():  # Check if it's a file
+
+                minute = file.name.split('.')[0].split('-')[-1]
+                minute_range = ["00", "15", "30", "45"]  # specifically opt into the timestamps...
+
+                if minute in minute_range:
+                    df = read_json_to_pd(file)
+                    li.append(df)
+
+
+        subway_all = pd.concat(li, axis=0, ignore_index=True)
+        subway_all.drop_duplicates(subset=['Station', 'hour', 'minute'], keep="first", inplace=True)
+
+        # make sure these are all for the right date
+        yyyy = int(date_str.split('-')[0])
+        dd = int(date_str.split('-')[-1])
+        mo = int(date_str.split('-')[1])
+        subway_all = subway_all[subway_all['year'] == yyyy] 
+        subway_all = subway_all[subway_all['month'] == mo]
+        subway_all = subway_all[subway_all['day'] == dd] 
+
+        orig_id_total = len(subway_all['Station'].unique()) # to calculate percentage
+
+        # ------------------------------------------------ #
+        # VALIDATION
+        # Check if each ID has 24hrs of info
+        # ingesting more data is good -- sometimes records missing, ends up taking the average over an hour
+        # ------------------------------------------------ #
+
+        # every carpark record needs to have at least 1 record for each of the 24hrs
+        subway_all.drop_duplicates(subset=['Station', 'hour', 'minute'], keep="first")
+
+        to_remove = subway_all.drop_duplicates(subset=['Station', 'hour'], keep="first")
+        to_remove = to_remove.groupby(['Station']).size()
+        to_remove = pd.DataFrame(to_remove)
+        to_remove = to_remove[to_remove[0] < 24]
+        to_remove = to_remove.reset_index()
+        
+        # Filter subway_all to keep only rows where carpark id is NOT in ids_to_remove
+        subway_all = subway_all[~subway_all['Station'].isin(to_remove['Station'])].reset_index(drop=True)
+        
+        # check percentage of 'good' records
+        new_id_total = len(subway_all['Station'].unique())
+        percent_good = new_id_total/orig_id_total
+        
+        print("Subway stations kept: " + str(new_id_total) + "/" + str(orig_id_total))
+
+        # exit the process if data isn't good enough
+        if percent_good < tolerance:
+            sys.exit("This subway dataset is " + str(percent_good) + " good The tolerance is set to " + str(tolerance) + ".")
+        
+        # ------------------------------------------------ #
+        # CLEAN SUBWAY DF
+        # ------------------------------------------------ #
+
+        # standardize column names
+        subway_all = subway_all.rename(columns={'Station': 'station_code', 
+                                'CrowdLevel': 'crowd_level'})
+
+        # drop unnecessary columns
+        subway_all = subway_all.drop(columns=['StartTime', 'EndTime'])
+        subway_all['crowd_level'] = subway_all['crowd_level'].map({'l': 0, 'm': 0.5, 'h': 1}) #remap crowdedness to numbers
+
+
+        subway_all = subway_all.dropna(subset=['station_code'])
+        subway_all['crowd_level'] = subway_all['crowd_level'].fillna(0)
+
+
+        # ------------------------------------------------ #
+        # IMPORT REFERENCE OF STATION-KML 
+        # ------------------------------------------------ #
+
+        # triggers a ton of warnings but works
+        station_ref = pd.read_csv(path_to_subway_kml)  
+        subway_kml = station_ref[['station_code', 'station_name', 'subzone_kml', 'region_name', 'region_num']]
+        subway_all = subway_all.merge(subway_kml, on="station_code")
+
+        # ------------------------------------------------ #
+        # MERGE AND SUMMARIZE SUBWAY CROWDEDNESS PER KML
+        # ------------------------------------------------ #
+        subway_grouped = subway_all.groupby(['subzone_kml', 'hour'])
+        subway_summary = subway_grouped.agg(crowd_mean = ('crowd_level', 'mean'))
+        subway_summary = pd.DataFrame(subway_summary)
+        subway_summary = subway_summary.reset_index()
+        
+        # add LED vals by remapping normalized val of 0 - 1 to 0-255 (inverted -- so more avail is more dim)
+        subway_summary['subway_led_val'] = subway_summary['crowd_mean'] * 255 
+        subway_summary['subway_led_val'] = subway_summary['subway_led_val'].astype('int64')
         
         # ------------------------------------------------ #
         # IMPORT CSV REFERENCE OF LEDS-KMLS & MERGE
         # ------------------------------------------------ #
         led_ref = pd.read_csv(path_to_subzone_kml_ref_by_region)
         
-        # merge right to preserve ALL LED #s
+        # merge with carparks, merge right to preserve ALL LED #s
         # All leds / kmls without hourly lot data will have only 1 row of NAs
-        summary_df = summary_df.merge(led_ref, how="right", on="subzone_kml")
+        summary_df = carpark_summary.merge(led_ref, how="right", on="subzone_kml")
         
+        # merge with subways; similarly may have NAs
+        summary_df = summary_df.merge(summary_df, how="right")
+
         # ------------------------------------------------ #
         # CLEAN & REORDER DATAFRAME
         # ------------------------------------------------ #
-        # fill NAs with 0s and recast floats to ints
+        # fill NAs with 0s 
         summary_df['led_val'] = summary_df['led_val'].fillna(0)
+        summary_df['subway_led_val'] = summary_df['subway_led_val'].fillna(0)
         summary_df['hour'] = summary_df['hour'].fillna(0)
         summary_df['avail_norm'] = summary_df['avail_norm'].fillna(0)
+        summary_df['crowd_mean'] = summary_df['crowd_mean'].fillna(0)
+        
+        # recast floats to ints
         summary_df['led_val'] = summary_df['led_val'].astype('int64')
+        summary_df['subway_led_val'] = summary_df['subway_led_val'].astype('int64')
         summary_df['hour'] = summary_df['hour'].astype('int64')
 
         # sort by region, led# and hour
         summary_df = summary_df.sort_values(by=['region_no', 'led_no', 'hour'])
 
         # reorder
-        summary_df = summary_df[['region_no', 'region_n', 'led_no', 'hour', 'avail_norm', 'led_val', 'subzone_no', 'subzone_n', 'subzone_kml']]
+        summary_df = summary_df[['region_no', 'region_n', 'led_no', 'hour', 'avail_norm', 'crowd_mean', 'led_val', 'subway_led_val', 'subzone_no', 'subzone_n', 'subzone_kml']]
         summary_df = summary_df.reset_index(drop=True)
 
         # ------------------------------------------------ #
@@ -248,18 +358,20 @@ if __name__ == "__main__":
         
         df = df.groupby(['region_n', 'region_no', 'led_no','subzone_no', 'subzone_n', 'subzone_kml']).agg(
             hour = ('hour', f),  
-            led_val = ('led_val', f),   
+            led_val = ('led_val', f), 
+            subway_led_val = ('subway_led_val', f),  
             avail_norm = ('avail_norm', f),   
+            crowd_mean = ('crowd_mean', f),
         ).reset_index().reindex(df.columns, axis=1)
 
         # choose cols Matthew needs
         df = df[['region_n', 'led_no', 'led_val', 'subzone_n', 'subzone_kml']] 
 
         # rename to Matthew's json format
-        df = df.rename(columns={'led_no': 'led_number', 'led_val': 'carpark', 'subzone_n': 'name', 'subzone_kml': 'kml'})
+        df = df.rename(columns={'led_no': 'led_number', 'led_val': 'carpark', 'subway_led_val': 'mrt', 'subzone_n': 'name', 'subzone_kml': 'kml'})
         
         # Group by region and turn rows into a list of dictionaries for each group
-        df = df.groupby(['region_n'])[['led_number', 'carpark', 'name', 'kml']].apply(lambda x: x.to_dict(orient='records'))
+        df = df.groupby(['region_n'])[['led_number', 'carpark', 'mrt', 'name', 'kml']].apply(lambda x: x.to_dict(orient='records'))
         df = df.reset_index()
 
         # rename cols of new df shape

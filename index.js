89d5602ea1ccacb57a985e219139fe2fd8c08887
Zer02z2 <zongzechan@gmail.com>
2025-08-23 13:32:20 -0700

    base path update

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 // Express app for scheduling node-cron job
 
-
 /* -------------------- */
 /* SETUP      
 /* -------------------- */
 
-const fs = require('fs');
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
+const fs = require("fs")
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT || 6001;
-app.use(express.json());
+const app = express()
+const port = process.env.PORT || 6001
+app.use(express.json())
 
 // Node-cron scheduler, which continues running in background
 // Pulls and saves data every hour
-require("./server/scheduler");
+require("./server/scheduler")
 
 // Help with time formating
-const sgtime = require("./server/sgtime");
-
-
+const sgtime = require("./server/sgtime")
 
 /* -------------------- */
 /* SERVER        
 /* -------------------- */
 app.listen(port, async () => {
-    console.log("Server running at port: " + port);
-
+  console.log("Server running at port: " + port)
 })
 
+app.get("/", async (req, res) => {
+  res.status(200).json({ status: "online" })
+})
 
 // GET JSON
-app.get('/hello', async (req, res) => {
-  console.log("/hello");
+app.get("/hello", async (req, res) => {
+  console.log("/hello")
 
-  const yesterday = new Date(Date.now() - 86400000);
-  const yyyymmdd = sgtime.getYYYYMMDD(yesterday);
-  const filePath = __dirname + `/output/json/${yyyymmdd}.json`;
+  const yesterday = new Date(Date.now() - 86400000)
+  const yyyymmdd = sgtime.getYYYYMMDD(yesterday)
+  const filePath = __dirname + `/output/json/${yyyymmdd}.json`
 
   // if no file exists for date string, serve up a hard-coded file with good data
-  const contingency = '2025-08-19' // fake it with a known 'good day'
+  const contingency = "2025-08-19" // fake it with a known 'good day'
 
   fs.access(filePath, fs.constants.F_OK, (err) => {
-      if (err) {
-          console.error(`${filePath} does not exist. Serving ${contingency} instead.\n`, err);
-          res.sendFile(__dirname + `/output/json/${contingency}.json`);
-      } else {
-          res.sendFile(filePath);
-      }
-  });
-
-});
-
+    if (err) {
+      console.error(
+        `${filePath} does not exist. Serving ${contingency} instead.\n`,
+        err
+      )
+      res.sendFile(__dirname + `/output/json/${contingency}.json`)
+    } else {
+      res.sendFile(filePath)
+    }
+  })
+})

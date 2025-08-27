8c123813046f89ad358a280844bfadb593d4eac8
rhodamined <rhodamined@gmail.com>
2025-08-27 17:06:06 -0400

    add endpoints by hour

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 /* SETUP      
 /* -------------------- */
 
-const fs = require("fs")
+const fse = require("fs-extra")
 const express = require("express")
 const dotenv = require("dotenv")
 dotenv.config()
@@ -31,7 +31,7 @@ app.get("/", async (req, res) => {
   res.status(200).json({ status: "online" })
 });
 
-// GET JSON
+// GET JSON FOR ALL KML / LEDS
 app.get("/hello", async (req, res) => {
   console.log("/hello")
 
@@ -42,7 +42,7 @@ app.get("/hello", async (req, res) => {
   // if no file exists for date string, serve up a hard-coded file with good data
   const contingency = "2025-08-19" // fake it with a known 'good day'
 
-  fs.access(filePath, fs.constants.F_OK, (err) => {
+  fse.access(filePath, fse.constants.F_OK, (err) => {
     if (err) {
       console.error(
         `${filePath} does not exist. Serving ${contingency} instead.\n`,
@@ -56,6 +56,76 @@ app.get("/hello", async (req, res) => {
 });
 
 
+// GET BY HOUR;; /hour/0 through /hour/23
+app.get('/hour/:hour', async (req, res) => {
+  const hour = parseInt(req.params.hour);
+
+  // Return error if invalid hour
+  if (hour < 0 || hour > 23) {
+    return res.status(404).send(`${hour} is not a valid endpoint. Use an hour from 0 to 23. `); 
+  }
+
+  // yesterday
+  const yesterday = new Date(Date.now() - 86400000)
+  const yyyymmdd = sgtime.getYYYYMMDD(yesterday)
+  const filePath = __dirname + `/output/json/${yyyymmdd}.json`
+
+  const contingency = "2025-08-19" // if we need to, fake it with a known 'good day'
+  
+  // Check if file exists
+  fse.access(filePath, fse.constants.F_OK, (err) => {
+
+    // if it doesn't exist, serve up the contingency file
+    if (err) {
+      console.error(`${filePath} does not exist. Serving ${contingency} instead.\n`, err)
+
+      // Read the file and serve up just 1 hour of it
+      // Serving only 1 hour because Arduino cannot save all 24hrs to memory at one time
+      fse.readJson(__dirname + `/output/json/${contingency}.json`, (err, payload) => {
+        if (err) {
+          console.error(err)
+        } 
+        else {
+          for (let region of payload.regions) {
+            for (let sz of region.subzones) { // array indices correspond to LED
+              sz.carpark = sz.carpark[hour];
+              sz.mrt = sz.mrt[hour];
+            }
+          }
+          
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(payload));
+
+        }
+      })
+
+    } else {
+      
+      // Default behavior: read original file path, do same as above
+      fse.readJson(filePath, (err, payload) => {
+        if (err) {
+          console.error(err)
+        } 
+        else {
+          for (let region of payload.regions) {
+            for (let sz of region.subzones) { // array indices correspond to LED
+              sz.carpark = sz.carpark[hour];
+              sz.mrt = sz.mrt[hour];
+            }
+          }
+          
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(payload));
+
+        }
+      })
+
+    }
+  })
+
+});
+
+
 // GET DATA LOG (PULLING FROM API)
 app.get("/datalog", async (req, res) => {
   console.log("/datalog")
@@ -63,7 +133,7 @@ app.get("/datalog", async (req, res) => {
   const filePath = __dirname + `/data/log.txt`
 
 
-  fs.access(filePath, fs.constants.F_OK, (err) => {
+  fse.access(filePath, fse.constants.F_OK, (err) => {
     if (err) {
       console.error(
         `${filePath} does not exist.`,
@@ -82,7 +152,7 @@ app.get("/outputlog", async (req, res) => {
   const filePath = __dirname + `/output/log.txt`
 
 
-  fs.access(filePath, fs.constants.F_OK, (err) => {
+  fse.access(filePath, fse.constants.F_OK, (err) => {
     if (err) {
       console.error(
         `${filePath} does not exist.`,

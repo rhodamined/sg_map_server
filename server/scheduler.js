efa451e6949c4745817e15ddd44527829ed0b872
rhodamined <rhodamined@gmail.com>
2025-08-27 22:50:15 -0400

    schedule

--- server/scheduler.js
+++ server/scheduler.js
@@ -50,7 +50,7 @@ cron.schedule('55 23 * * *', clearDataLog);
 
 // for testing only
 // Every day at 00:01, process entire previous day's worth of data into a csv and json
-cron.schedule('4 2 * * *', runPythonScript);
+cron.schedule('52 2 * * *', runPythonScript);
 
 
 /* ------------------------------------------------ */
